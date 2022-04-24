f16fd87a7a7b0f123991fe0b1d8b54972192fffc
Mariano Huecke <85529246+Tatino12@users.noreply.github.com>
2022-04-24 20:28:26 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x0FAE16FFce9Af7755DB100805b68F765AaFFc2Df';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
